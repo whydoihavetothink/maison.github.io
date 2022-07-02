3cec872508926a7a5b1e9e4f39dc062b3407350e
bhtrung <tonik8765@gmail.com>
2022-07-02 21:54:25 +0200

    small changes
    laguage in menu
    phone layout adjustments

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ const navLinks = document.querySelector(".nav--links");
 const navToggle = document.querySelector(".nav--toggle");
 var navBurger = document.querySelector(".nav--toggle div");
 var navLinkItems = document.querySelector(".nav--links li");
+const body = document.querySelector("body");
 
 const mainColor = "#4b5320";
 const secondaryColor = "#d6a559";
@@ -19,33 +20,18 @@ navToggle.addEventListener("click", function(){
          i+=1;                                              //animation timing
          navLinkItems.style.animation = "navLinkItemsFade "+(i/5+0.3)+"s ease";
       }
-      //color reset (so the color doesnt change for mobile devices)
-      for(navBurger of navToggle.children){
-         navBurger.style.backgroundColor = secondaryColor;
-      }
+      //disables scrolling in menu for mobile
+      body.classList.add("fixed-position");
 
    } else { //data visibility is visible
       navLinks.setAttribute("data-visibility", "hidden");
+
+      // enables scrolling
+      body.classList.remove("fixed-position");
+      
       // animation reset
       for(navLinkItems of navLinks.children){
          navLinkItems.style.animation = "";
       }
-      //color reset (so the color doesnt change for mobile devices)
-      for(navBurger of navToggle.children){
-         navBurger.style.backgroundColor = secondaryColor;
-      }
-   }
-
-})
-
-// mouse hover animation over burger
-navToggle.addEventListener("mouseenter", function () {
-   for(navBurger of navToggle.children){
-      navBurger.style.backgroundColor = "red";
-   }
-})
-navToggle.addEventListener("mouseleave", function () {
-   for(navBurger of navToggle.children){
-      navBurger.style.backgroundColor = secondaryColor;
    }
 })
