460393439b9e7e91c0ba84aa5696bc73423e4703
bhtrung <tonik8765@gmail.com>
2022-07-11 18:32:21 +0200

    touches for mobille
    bottom bar disappears on menu open
    instagram link fix

--- App.js
+++ App.js
@@ -2,10 +2,14 @@ const navLinks = document.querySelector(".nav--links");
 const navToggle = document.querySelector(".nav--toggle");
 var navBurger = document.querySelector(".nav--toggle div");
 var navLinkItems = document.querySelector(".nav--links li");
+var phoneIcons = document.querySelector(".phone-icons");
+var iconsContainer = document.querySelector("#icons-container");
 const body = document.querySelector("body");
 
 const mainColor = "#4b5320";
 const secondaryColor = "#d6a559";
+
+
 
 navToggle.addEventListener("click", function(){
    // from burger to x toggle
@@ -23,12 +27,25 @@ navToggle.addEventListener("click", function(){
       //disables scrolling in menu for mobile
       body.classList.add("fixed-position");
 
+      //hides bottom navigation on mobile
+      for(phoneIcons of iconsContainer.children){
+         phoneIcons.style.display = "none";
+         iconsContainer.style.border = "none";
+      }
+      
+
    } else { //data visibility is visible
       navLinks.setAttribute("data-visibility", "hidden");
 
       // enables scrolling
       body.classList.remove("fixed-position");
-      
+
+      //shows bottom navigation on mobile
+      for(phoneIcons of iconsContainer.children){
+         phoneIcons.style.display = "block";
+         iconsContainer.style.border = "3px solid var(--secondary-color)";
+      }
+
       // animation reset
       for(navLinkItems of navLinks.children){
          navLinkItems.style.animation = "";
